1749bc1920c00182bb4600b97785fb86eb206636
chutified <tommychu2256@gmail.com>
2020-12-13 00:04:52 +0100

    Add docker to automatization

--- setup.sh
+++ setup.sh
@@ -14,7 +14,6 @@ echo "Linux sofware setup"
 echo "==========================="
 
 # GIT
-echo "==> Installing git..."
 sudo dnf install git --assumeyes --nodocs
 
 # GIT CONFIGURATION
@@ -40,7 +39,6 @@ sudo sh -c 'echo "fastestmirror=true" >> /etc/dnf/dnf.conf'
 sudo sh -c 'echo "deltarpm=true" >> /etc/dnf/dnf.conf'
 
 # UPDATE
-echo "=== Updating installed packages... ==="
 sudo dnf update --assumeyes --nodocs
 
 # BATTERY USAGE
@@ -53,80 +51,79 @@ sudo cp bashr /root/.bashrc
 
 # PROGRAMMING LANGUAGES/UTILITIES
 # python
-echo ""
-echo "==> installing python..."
 sudo dnf install python --assumeyes --nodocs
 sudo dnf install python3 --assumeyes --nodocs
 # nodejs
-echo ""
-echo "==> installing nodejs..."
 sudo dnf install nodejs --assumeyes --nodocs
 # golang
-echo ""
-echo "==> installing golang..."
 sudo dnf install golang --assumeyes --nodocs
 mkdir ~/go
 mkdir ~/go/src ~/go/bin ~/go/pkg
 # postgres
-echo ""
-echo "==> installing postgres..."
 sudo dnf install postgresql --assumeyes --nodocs
 # ctags
-echo ""
-echo "==> installing ctags..."
 sudo dnf install ctags --assumeyes --nodocs
 
 # UI
-echo ""
-echo "==> installing gnome-tweaks..."
 sudo dnf install gnome-tweaks --assumeyes --nodocs
 
 # IMAGES
 # raster graphics
-echo ""
-echo "==> installing gimp..."
 sudo dnf install gimp --assumeyes --nodocs
 # vector graphics
-echo ""
-echo "==> installing inkscape..."
 sudo dnf install inkscape --assumeyes --nodocs
 
 # MEDIA
 # video format
-echo ""
-echo "==> installing vlc..."
 sudo dnf install vlc --assumeyes --nodocs
 # installing apps
-echo ""
-echo "==> installing transmission"
 sudo dnf install transmission --assumeyes --nodocs
 # google chrome browser
-echo ""
-echo "==> installing google-chrome..."
 sudo dnf install google-chrome --assumeyes --nodocs
 # dropbox
 sudo dnf install dropbox nautilus-dropbox --assumeyes --nodocs
 
 # NEOVIM
-echo ""
-echo "==> installing neovim..."
 sudo dnf install neovim --assumeyes --nodocs
+# vim-plug
+sudo sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 # htop
-echo ""
-echo "==> installing htop..."
 sudo dnf install htop --assumeyes --nodocs
 # fuzzy finder
-echo ""
-echo "==> installing fzf..."
 sudo dnf install fzf --assumeyes --nodocs
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+# neovim language support
+sudo npm install -g neovim
+pip3 install pynvim
+
+# DOCKER
+sudo dnf remove --asumeyes docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io --asumeyes
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker run hello-world
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
 # CLEAN UP
 sudo dnf autoremove --assumeyes
 
 # TODO
-echo ""
 echo "=== TODO ==="
-echo "==> Manually install Docker, NERD-FiraCode, Dash-to-dock"
-echo "==> Configure NeoVim, Postgres, Dash-to-dock, Dropbox, Chrome, GNOME-Tweak"
+echo "==> Manually install NERD-FiraCode, Dash-to-dock"
+echo "==> Configure NeoVim, Postgres, Dash-to-dock, Dropbox, Chrome, GNOME-Tweak, Settings, Terminal"
