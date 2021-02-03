607bd256310f4d4317b2d10ffb8fb6a8f63a9066
chutified <tommychu2256@gmail.com>
2021-02-03 10:27:59 +0100

    UBUNTU setup

---
+++ ubuntu-setup.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# === SETUP ==
+
+# MAINTAINER:   Tommy Chu
+# GITHUB:       chutified
+# EMAIL:        tommychu2256@gmail.com
+
+# SET
+clear
+echo "========================="
+echo "| Ubuntu software setup |"
+echo "========================="
+
+# APT
+sudo apt update
+sudo apt -y upgrade
+
+# SETTINGS
+sudo cp bashrc ~/.bashrc
+sudo cp bashrc /root/.bashrc
+sudo cp vimrc ~/.vimrc
+sudo cp vimrc /root/.vimrc
+
+# GIT
+sudo apt install git-all
+git config --global user.name "chutified"
+git config --global user.email "tommychu2256@gmail.com"
+
+# PERFORMANCE
+sudo apt install preload
+
+# BATTERY
+sudo apt install tlp tlp-rdw
+sudo tlp start
+sudo apt install tp-smapi-dkms acpi-call-dkms
+
+# LANGUAGES
+sudo apt install nodejs
+sudo apt install npm
+sudo snap install go --classic
+mkdir ~/go
+mkdir ~/go/src ~/go/bin ~/go/pkg
+
+# APPs
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:inkscape.dev/stable
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+sudo apt update
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+sudo apt install vim
+sudo apt install gnome-tweak-tool
+sudo apt install inkscape
+sudo snap install gimp
+sudo snap install vlc
+sudo snap install goland --classic
+sudo snap install postman
+sudo snap install teams
+
+# DOCKER
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+# CLEAN UP
+sudo apt -y upgrade
+sudo apt autoremove
+
+echo "=== TODO ==="
+echo "1. Install/config Chrome, Dropbox, Terminal, Dash To Dock"
+echo "2. Config Software and updates, Wireless DNS, MS Teams, Goland, Postman, Git SSH"
+echo "3. Settings, GNOME Tweaks"
