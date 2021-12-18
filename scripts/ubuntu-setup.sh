934b28c7be9fa11111b01f4900f93ec499dd8e22
chutommy <tommychu2256@gmail.com>
2021-12-18 12:38:19 +0100

    Fix docker installation

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -57,24 +57,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo groupadd docker
-sudo usermod -aG docker $USER
-
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get -y install \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  gnupg-agent \
-  software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo docker run hello-world
 sudo groupadd docker
