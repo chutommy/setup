8e83b576e227308beef0724998a1196f1f46fa48
chutommy <tommychu2256@gmail.com>
2021-12-18 11:44:11 +0100

    Update ubuntu system setup script

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -1,72 +1,66 @@
 #!/bin/bash
 
-# === SETUP ==
-
 # MAINTAINER:   Tommy Chu
 # GITHUB:       chutommy
 # EMAIL:        tommychu2256@gmail.com
 
-# SET
-clear
-echo "========================="
-echo "| Ubuntu software setup |"
-echo "========================="
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
 
-# APT
-sudo apt update
-sudo apt -y upgrade
+# rcs
+sudo cp ../rcs/bashrc ~/.bashrc
+sudo cp ../rcs/bashrc /root/.bashrc
+sudo cp ../rcs/vimrc ~/.vimrc
+sudo cp ../rcs/vimrc /root/.vimrc
 
-# SETTINGS
-sudo cp bashrc ~/.bashrc
-sudo cp bashrc /root/.bashrc
-sudo cp vimrc ~/.vimrc
-sudo cp vimrc /root/.vimrc
+sudo apt-get -y update
 
-# GIT
-sudo apt -y install git-all
+# git
+sudo apt-get -y install git-all
 git config --global user.name "chutommy"
 git config --global user.email "tommychu2256@gmail.com"
 
-# PERFORMANCE
-sudo apt -y install preload
-
-# BATTERY
-sudo apt -y install tlp tlp-rdw
+# performance & battery
+sudo apt-get -y install preload
+sudo apt-get -y install tp-smapi-dkms acpi-call-dkms
+sudo apt-get -y install tlp tlp-rdw
 sudo tlp start
-sudo apt -y install tp-smapi-dkms acpi-call-dkms
 
-# LANGUAGES
-sudo apt -y install nodejs
-sudo apt -y install npm
-sudo snap install go --classic
-mkdir ~/go
-mkdir ~/go/src ~/go/bin ~/go/pkg
+# languages
+sudo apt-get -y install python3
+sudo apt-get -y install golang
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
 
-# APPs
-sudo add-apt-repository universe
-sudo add-apt-repository ppa:inkscape.dev/stable
-sudo add-apt-repository ppa:otto-kesselgulasch/gimp
-sudo apt update
-
+# browser
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt -y install ./google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-sudo apt -y install curl
-sudo apt -y install vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-sudo apt -y install gnome-tweak-tool
-sudo apt -y install inkscape
-sudo apt -y install flameshot
-sudo snap install gimp
-sudo snap install vlc
-sudo snap install goland --classic
+# apps
+sudo apt-get -y install curl
+sudo apt-get -y install vim
+sudo apt-get -y install neovim
+sudo apt-get -y install trash-cli
+sudo apt-get -y install inkscape
+sudo apt-get -y install gimp
+sudo apt-get -y install gnome-tweak-tool
 sudo snap install postman
-sudo snap install teams
-sudo snap install htop
+sudo snap install vlc
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install deja-dup --classic
 
 # DOCKER
+sudo apt-get -y install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get -y install \
   apt-transport-https \
@@ -88,21 +82,7 @@ sudo usermod -aG docker "$USER"
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
-# CLEAN UP
-sudo apt -y upgrade
-sudo apt autoremove
-
-echo "=== TODO ==="
-echo "1. Install/config Chrome, Dropbox, Terminal, Dash To Dock"
-echo "2. Config Software and updates, Wireless DNS, MS Teams, Goland, Postman, Git SSH"
-echo "3. Settings, GNOME Tweaks"
-
-echo """
-  Keyboard Shortcuts
-  ------------------
-  Open Google Chrome [google-chrome] (Super+A)
-  Open File manager [nautilus] (Super+Q)
-  Open Goland [goland] (Super+W)
-  Take Flameshot [flameshot gui] (Super+Z)
-  Play LoFi [gnome-terminal -x sh -c \"mpg123 -v -l 0 -g 5 -K ~/Dropbox/Music/codefi.mp3; bash\"] (Super+F)
-"""
+# final
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
