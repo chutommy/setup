736a004a2f8b0b1c2de220e4857ec8d1c588a076
Tommy Chu <tommychu2256@gmail.com>
2022-10-26 18:32:06 +0200

    Update ubuntu-setup.sh

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -19,7 +19,6 @@ git config --global user.name "chutommy"
 git config --global user.email "tommychu2256@gmail.com"
 
 # performance
-sudo apt-get -y install ubuntu-restricted-extras
 sudo apt-get -y install preload
 sudo systemctl enable fstrim.timer
 
@@ -27,6 +26,14 @@ sudo systemctl enable fstrim.timer
 sudo apt-get -y install tp-smapi-dkms acpi-call-dkms
 sudo apt-get -y install tlp tlp-rdw
 sudo systemctl enable tlp.service
+
+# firewall
+sudo ufw enable
+
+# ubuntu restricted extras
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
+sudo apt-get install ttf-mscorefonts-installer
+sudo apt-get -y install ubuntu-restricted-extras
 
 # customization
 sudo apt-get -y install gnome-tweak-tool
